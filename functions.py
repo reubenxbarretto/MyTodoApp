cae9584e5bf82c9498edf082a376b81788fb087b
Reuben <reuben.barretto8@gmai.com>
2024-06-27 11:49:07 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+FILEPATH = "TODO.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and
+    return a list of to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to_do items
+    in the text file
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
